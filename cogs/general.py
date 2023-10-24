1c743f6e08997f0a13636c164291f67870374fad
doge229 <doge229@gmail.com>
2023-10-24 15:10:48 -0500

    moved general.py and tag.py back into /cogs

---
+++ cogs/general.py
@@ -0,0 +1,177 @@
+# general.py
+
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ext.commands import Greedy
+import system
+import messages
+import random
+
+
+class General(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        super().__init__()
+
+    async def objmaplinkcreator(context, arg):
+        ANSWER = ''
+        LAYER = 'Surface'
+
+        TERMS = arg
+        TERMS = TERMS.replace(" ", "%20")
+        TERMS = TERMS.replace('"', "%22")
+        TERMS = TERMS.replace(":", "%3A")
+        TERMS = TERMS.replace("'", "%20")
+        TERMS = TERMS.replace("-", "%20")
+
+        if 'sky' in TERMS.lower() and not 'not sky' in arg.lower():
+            LAYER = 'Sky'
+        elif 'depths' in TERMS.lower() and not 'not depths' in arg.lower():
+            LAYER = 'Depths'
+
+        LINK = messages.COMMAND_OBJMAPBASELINK + LAYER + '?q=' + TERMS
+        ANSWER = messages.COMMAND_OBJMAPTERMS + '`' + arg + '`:\n' + LINK + '\n' + messages.COMMAND_OBJMAPNOTE
+
+        await system.respond(context, ANSWER)
+
+    async def coordconverter(context, arg):
+        ANSWER = ''
+
+        try:
+            OBJMAPCOORDS = arg.lower().strip()
+            objcoords = OBJMAPCOORDS.split(", ")
+
+            Xcoord = round(float(objcoords[0]))
+            Zcoord = round(float(objcoords[1])) - 105
+            Ycoord = round(float(objcoords[2]))
+
+            showncoords = str(Xcoord) + ', ' + str(Ycoord) + ', ' + str(Zcoord)
+
+            ANSWER = messages.CONVERT_COORD1 + '`' + arg + '`' + messages.CONVERT_COORD2 + '`' + showncoords + '`'
+        except:
+            ANSWER = messages.ERROR_BADCOORDS
+
+        await system.respond(context, ANSWER)
+
+    async def findpristine(context, arg):
+        ANSWER = ''
+
+        TERMS = arg
+        TERMS = TERMS.replace(" ", "%20")
+        TERMS = TERMS.replace('"', "%22")
+        TERMS = TERMS.replace(":", "%3A")
+        TERMS = TERMS.replace("'", "%20")
+        TERMS = TERMS.replace("-", "%20")
+
+        LINK = messages.COMMAND_OBJMAPBASELINK +'Depths?q=minusfieldghost%20' + TERMS
+        ANSWER = messages.COMMAND_FINDPRISTINE1 + '`' + arg + '`:\n' + LINK
+
+        await system.respond(context, ANSWER)
+
+    async def finddispenser(context, arg):
+        ANSWER = ''
+        DISPENSERS = ''
+        VALID = True
+        TERM = arg
+
+        arg = system.argcleanup(arg)
+
+        match arg:
+            case 'fan':
+                DISPENSERS = messages.DISP_FAN
+            case 'wing':
+                DISPENSERS = messages.DISP_WING
+            case 'cart':
+                DISPENSERS = messages.DISP_CART
+            case 'balloon':
+                DISPENSERS = messages.DISP_BALLOON
+            case 'rocket':
+               DISPENSERS = messages.DISP_ROCKET
+            case 'timebomb':
+                DISPENSERS = messages.DISP_TIMEBOMB
+            case 'portablepot':
+                DISPENSERS = messages.DISP_PORTABLEPOT
+            case 'flameemitter':
+                DISPENSERS = messages.DISP_FLAMEEMITTER
+            case 'frostemitter':
+                DISPENSERS = messages.DISP_FROSTEMITTER
+            case 'shockemitter':
+                DISPENSERS = messages.DISP_SHOCKEMITTER
+            case 'beamemitter':
+                DISPENSERS = messages.DISP_BEAMEMITTER
+            case 'hydrant':
+                DISPENSERS = messages.DISP_HYDRANT
+            case 'steeringstick':
+                DISPENSERS = messages.DISP_STEERINGSTICK
+            case 'bigwheel':
+                DISPENSERS = messages.DISP_BIGWHEEL
+            case 'smallwheel':
+                DISPENSERS = messages.DISP_SMALLWHEEL
+            case 'sled':
+                DISPENSERS = messages.DISP_SLED
+            case 'battery':
+                DISPENSERS = messages.DISP_BATTERY
+            case 'bigbattery':
+                VALID = False
+                ANSWER = messages.DISP_BIGBATTERY
+            case 'spring':
+                DISPENSERS = messages.DISP_SPRING
+            case 'cannon':
+                DISPENSERS = messages.DISP_CANNON
+            case 'stabilizer':
+                DISPENSERS = messages.DISP_STABILIZER
+            case 'hoverstone':
+                DISPENSERS = messages.DISP_HOVERSTONE
+            case 'light':
+                DISPENSERS = messages.DISP_LIGHT
+            case 'stake':
+                DISPENSERS = messages.DISP_STAKE
+            case 'mirror':
+                DISPENSERS = messages.DISP_MIRROR
+            case 'homingcart':
+                DISPENSERS = messages.DISP_HOMINGCART
+            case 'constructhead':
+                DISPENSERS = messages.DISP_CONSTRUCTHEAD
+            case _:
+                VALID = False
+                ANSWER = messages.ERROR_UNKNOWNCMD
+
+        if VALID:
+            ANSWER = messages.DISP_BASE + f'`{TERM}`' + messages.DISP_BASE2 + f'\n' + DISPENSERS
+
+        await system.respond(context, ANSWER)
+
+    async def hello(context):
+        ANSWER = ''
+    
+        CHOICE = random.randint(1, 6)
+    
+        match CHOICE:
+            case 1:
+                ANSWER = messages.COMMAND_HELLO1
+            case 2:
+                ANSWER = messages.COMMAND_HELLO2
+            case 3:
+                ANSWER = messages.COMMAND_HELLO3
+            case 4:
+                if type(context) == discord.ext.commands.Context:
+                    ANSWER = f'{messages.COMMAND_HELLO1[:-1]} <@{context.author.id}>!'
+                elif type(context) == discord.Interaction:
+                    ANSWER = f'{messages.COMMAND_HELLO1[:-1]} <@{context.user.id}>!'
+            case 5:
+                if type(context) == discord.ext.commands.Context:
+                    ANSWER = f'{messages.COMMAND_HELLO2[:-1]} <@{context.author.id}>!'
+                elif type(context) == discord.Interaction:
+                    ANSWER = f'{messages.COMMAND_HELLO2[:-1]} <@{context.user.id}>!'
+            case 6:
+                if type(context) == discord.ext.commands.Context:
+                    ANSWER = f'{messages.COMMAND_HELLO3[:-1]} <@{context.author.id}>!'
+                elif type(context) == discord.Interaction:
+                    ANSWER = f'{messages.COMMAND_HELLO3[:-1]} <@{context.user.id}>!'
+        
+        await system.respond(context, ANSWER)
+
+
+async def setup(bot):
+    await bot.add_cog(General(bot))
--- cogs/generalcalls.py
+++ cogs/generalcalls.py
@@ -3,10 +3,10 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import system
-import tag
-import general
+from .general import General
+from .tag import Tag
 
-class General2(commands.Cog):
+class GeneralCalls(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         super().__init__()
@@ -15,64 +15,64 @@ class General2(commands.Cog):
     @commands.guild_only()
     @commands.check(system.check_banned)
     async def tag(self, ctx, *, arg):
-        await tag.tag(context=ctx, arg=arg)
+        await Tag.tag(context=ctx, arg=arg)
     
     @app_commands.command(name='tag', description='This is my main command for information! Try using /tag help!')
     @app_commands.check(system.slashcheck_banned)
     async def tagslash(self, interaction: discord.Interaction, *, option: str):
-        await tag.tag(context=interaction, arg=option)
+        await Tag.tag(context=interaction, arg=option)
 
     
     @commands.command(name='createobjlink', aliases=['createobjectlink', 'find'])
     @commands.guild_only()
     @commands.check(system.check_banned)
     async def objmaplinkcreator(self, ctx, *, arg):
-        await general.objmaplinkcreator(context=ctx, arg=arg)
+        await General.objmaplinkcreator(context=ctx, arg=arg)
 
     @app_commands.command(name='find', description='Create an Object Map link with your search terms integrated into it')
     @commands.guild_only()
     @app_commands.check(system.slashcheck_banned)
     async def objmaplinkcreatorslash(self, interaction: discord.Interaction, *, searchterms: str):
-        await general.objmaplinkcreator(context=interaction, arg=searchterms)
+        await General.objmaplinkcreator(context=interaction, arg=searchterms)
 
     
     @commands.command(name='coordconvert')
     @commands.guild_only()
     @commands.check(system.check_banned)
     async def coordconverter(self, ctx, *, arg):
-        await general.coordconverter(context=ctx, arg=arg)
+        await General.coordconverter(context=ctx, arg=arg)
     
     @app_commands.command(name='coordconvert', description='Copy the coordinates of an object on the Object Map here to convert them to in-game coords.')
     @commands.guild_only()
     @app_commands.check(system.slashcheck_banned)
     async def coordconverterslash(self, interaction: discord.Interaction, *, coordinates: str):
-        await general.coordconverter(context=interaction, arg=coordinates)
+        await General.coordconverter(context=interaction, arg=coordinates)
 
     
     @commands.command(name='findpristine')
     @commands.guild_only()
     @commands.check(system.check_banned)
     async def findpristine(self, ctx, *, arg):
-        await general.findpristine(context=ctx, arg=arg)
+        await General.findpristine(context=ctx, arg=arg)
 
     @app_commands.command(name='findpristine', description='Create an Object Map link that shows all the Depths Ghosts that can spawn a specific weapon')
     @commands.guild_only()
     @app_commands.check(system.slashcheck_banned)
     async def findpristineslash(self, interaction: discord.Interaction, *, weaponname: str):
-        await general.findpristine(context=interaction, arg=weaponname)
+        await General.findpristine(context=interaction, arg=weaponname)
 
     
     @commands.command(name='finddispenser', aliases=['dispenser'])
     @commands.guild_only()
     @commands.check(system.check_banned)
     async def finddispenser(self, ctx, *, arg):
-        await general.finddispenser(context=ctx, arg=arg)
+        await General.finddispenser(context=ctx, arg=arg)
     
     @app_commands.command(name='finddispenser', description='See which Device Dispensers can dispense a specific Zonai Device capsule')
     @commands.guild_only()
     @app_commands.check(system.slashcheck_banned)
     async def finddispenserslash(self, interaction: discord.Interaction, *, device: str):
-        await general.finddispenser(context=interaction, arg=device)
+        await General.finddispenser(context=interaction, arg=device)
 
 async def setup(bot):
-    await bot.add_cog(General2(bot))
+    await bot.add_cog(GeneralCalls(bot))
