9452c976cf1d024ad812bee0a5dc4c6415f2b15c
doge229 <doge229@gmail.com>
2023-12-08 14:03:56 -0600

    added cog to automatically change profile pic once per day

---
+++ cogs/dailyprofile.py
@@ -0,0 +1,43 @@
+# dailyprofile.py
+
+import os
+import datetime
+import pytz
+import inspect
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+import messages
+import system
+import config
+import json
+import random
+import asyncio
+
+timezone = datetime.timezone(datetime.timedelta(hours=-6))
+time = datetime.time(hour=8, minute=00, tzinfo=timezone)
+
+class DailyProfile(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        super().__init__()
+        self.testtimer.start()
+
+    def cog_unload(self):
+        self.testtimer.cancel()
+    
+    @tasks.loop(time=time)
+    async def testtimer(self):
+        int = random.randint(1, 100)
+        CHOICE = 'default'
+
+        if int == 1:
+            CHOICE = 'funny'
+        elif int > 51:
+            CHOICE = 'pride'
+
+        await system.setprofilepic(self.bot, CHOICE)
+         
+
+async def setup(bot):
+    await bot.add_cog(DailyProfile(bot))
