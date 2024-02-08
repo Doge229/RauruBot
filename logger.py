2176acbc84e740560c53b23bed8be805905575e8
doge229 <doge229@gmail.com>
2024-02-08 15:32:22 -0600

    createed logger.py file

---
+++ logger.py
@@ -0,0 +1,29 @@
+# logger.py
+
+import os
+import datetime
+import pytz
+import inspect
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+import messages
+import system
+import config
+import json
+import random
+import asyncio
+
+class Logger():
+    def __init__(self) -> None:
+        pass
+
+# create log file if not
+    
+# log function to be called, combine print with it
+    # error
+    # system
+    # admin
+    # dev?
+
+# way to send a log file though discord
